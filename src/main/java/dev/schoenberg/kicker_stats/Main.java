57acccb451038d27982c1b70c4ce0761061d44a8
Martin Schoenberg <martin.schoenberg@daimler.com>
2019-12-13 21:40:08 +0100

    Add flyway as db migration tool

--- src/main/java/dev/schoenberg/kicker_stats/Main.java
+++ src/main/java/dev/schoenberg/kicker_stats/Main.java
@@ -5,13 +5,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.flywaydb.core.Flyway;
+
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
 import dev.schoenberg.kicker_stats.core.helper.SimpleResourceLoader;
 import dev.schoenberg.kicker_stats.core.service.PlayerService;
 import dev.schoenberg.kicker_stats.persistence.helper.DaoRepository;
-import dev.schoenberg.kicker_stats.persistence.helper.TableInitializer;
 import dev.schoenberg.kicker_stats.persistence.repository.PlayerOrmLiteRepository;
 import dev.schoenberg.kicker_stats.rest.JettyServer;
 import dev.schoenberg.kicker_stats.rest.ServerService;
@@ -34,7 +35,9 @@ public class Main {
 		services.add(new PlayerController(new PlayerService(new PlayerOrmLiteRepository())));
 
 		try (JettyServer server = serverFactory.create(PORT, services); ConnectionSource connection = new JdbcConnectionSource(url)) {
-			TableInitializer.init(connection);
+			Flyway flyway = Flyway.configure().dataSource(url, null, null).load();
+			flyway.migrate();
+
 			DaoRepository.initDaos(connection);
 			server.run();
 		}
---
+++ src/main/resources/db/migration/V1__Create_player_table.sql
@@ -0,0 +1,6 @@
+create table players
+(
+   id UUID primary key NOT null,
+   name varchar (255) NOT null,
+   email varchar (255) NOT null
+);
