340db23757aa7a1775900283c2e56c176d9df581
Martin Schoenberg <martin.schoenberg@daimler.com>
2020-01-01 21:51:14 +0100

    Add unique constrain to mail

--- src/main/resources/db/migration/V1__Create_player_table.sql
+++ src/main/resources/db/migration/V1__Create_player_table.sql
@@ -1,10 +1,10 @@
 create table players
 (
-   id UUID primary key NOT null,
-   name varchar (255) NOT null,
-   mail varchar (255) NOT null,
-   password varchar (255) NOT null,
-   isAdmin boolean NOT null
+   id UUID primary key NOT NULL,
+   name varchar (255) NOT NULL,
+   mail varchar (255) NOT NULL UNIQUE,
+   password varchar (255) NOT NULL,
+   isAdmin boolean NOT NULL
 );
 
 INSERT INTO players (id, name, mail, password, isAdmin) VALUES ("0000000-0000-0000-0000-000000000001", "Super Admin", "admin@schoenberg.dev", "f38601176e77ab5945e54cbe71d7b18e7e8fbea024e895d427c9e1927e640037", true) ON CONFLICT DO NOTHING;
